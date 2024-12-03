644c6ed924179f6059b0fb242e5ea997bdb35c8d
Brent Scriver <bscriver@oneoddsock.com>
2024-12-02 21:03:09 -0500

    Add a means to generate action resource icons.

---
+++ Iconify/ActionResource.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using BG3Common;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Iconify
+{
+
+    internal partial class Program
+    {
+        enum IconType
+        {
+            Standard,
+            Highlight,
+            Missing,
+            Used
+        }
+
+        struct ActionResourceIconSetting
+        {
+            public string IconPath;
+            public int SideLength;
+            public IconType Type;
+            public bool Optional;
+        }
+
+        private static readonly ActionResourceIconSetting[] ActionResourceIconSettings = new ActionResourceIconSetting[]
+        {
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\Shared\\Resources\\{1}.DDS",
+                SideLength = 48,
+                Type = IconType.Standard,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\Shared\\Resources\\Highlight\\{1}.DDS",
+                SideLength = 48,
+                Type = IconType.Highlight,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\Shared\\Resources\\Missing\\{1}.DDS",
+                SideLength = 48,
+                Type = IconType.Missing,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\Shared\\Resources\\Used\\{1}.DDS",
+                SideLength = 48,
+                Type = IconType.Used,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\CC\\icons_resources\\{1}.DDS",
+                SideLength = 128,
+                Type = IconType.Standard,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\ActionResources_c\\Icons\\{1}.DDS",
+                SideLength = 80,
+                Type = IconType.Standard,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\ActionResources_c\\Icons\\Resources\\{1}.DDS",
+                SideLength = 64,
+                Type = IconType.Standard,
+                Optional = false
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\ActionResources_c\\Icons\\Resources\\Highlight\\{1}.DDS",
+                SideLength = 64,
+                Type = IconType.Highlight,
+                Optional = true
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\ActionResources_c\\Icons\\Resources\\Missing\\{1}.DDS",
+                SideLength = 64,
+                Type = IconType.Missing,
+                Optional = true
+            },
+            new ActionResourceIconSetting
+            {
+                IconPath = "Mods\\{0}\\GUI\\Assets\\ActionResources_c\\Icons\\Resources\\Used\\{1}.DDS",
+                SideLength = 64,
+                Type = IconType.Used,
+                Optional = true
+            }
+        };
+
+        /// <summary>
+        /// From https://mod.io/g/baldursgate3/r/implementing-custom-action-resources-with-impui
+        /// </summary>
+        /// <param name="inputPath">The path to the image file to generate action resource icons for.</param>
+        /// <param name="modPathRoot">The path to the root of the mod.</param>
+        /// <param name="actionResourceName">The name of the action resource to generate icons for.</param>
+        /// <returns></returns>
+        private static int CreateActionResourceIcons(string inputPath, string modPathRoot, string actionResourceName)
+        {
+            /* From the documentation:
+                Icons are in BC6H/BC7 format
+            
+               There are 4 different types of action resource icon:
+                1st type -Keyboard icon.Appears on top of hotbar, spellbook, reactions and in tooltips. Has 3 alternative versions for missing (it's been taken away),
+                    used (it's been expended) and unavailable.Size 48x48.
+                2nd type - Keyboard CC / level up icon.Appears in character creation or level up when a progression / passive grants an action resource.A progression
+                    description is also required. Size 128x128.
+                3rd type - Controller front icon.Appears on the left hand side of the action resource listing in both action radial "Cost" and "Action Resources"
+                    section.Size 80x80.
+                4th type - Controller resource icon.Appears on the right hand side of the action resource listing in both action radial "Cost" and "Action Resources"
+                    section. Has 3 OPTIONAL alternative versions for missing(it's been taken away), used (it's been expended) and unavailable.If you don't add a version
+                    for these, it'll look the same done for most of the base icons except action point, bonus action and movement.Size 44x64.
+            
+               Locations:
+                1st type - Keyboard icon:
+                    Mods\MODNAME\GUI\Assets\Shared\Resources\ACTIONRESOURCENAME.DDS
+                    Mods\MODNAME\GUI\Assets\Shared\Resources\Highlight\ACTIONRESOURCENAME.DDS
+                    Mods\MODNAME\GUI\Assets\Shared\Resources\Missing\ACTIONRESOURCENAME.DDS
+                    Mods\MODNAME\GUI\Assets\Shared\Resources\Used\ACTIONRESOURCENAME.DDS
+                 2nd type - Keyboard CC/level up icon:
+                    Mods\MODNAME\GUI\Assets\CC\icons_resources\ACTIONRESOURCENAME.DDS
+                 3rd type - Controller front icon:
+                    Mods\MODNAME\GUI\Assets\ActionResources_c\Icons\ACTIONRESOURCENAME.DDS
+                 4th type - Controller resource icon:
+                    Mods\MODNAME\GUI\Assets\ActionResources_c\Icons\Resources\ACTIONRESOURCENAME.DDS
+                 OPTIONAL
+                    Mods\MODNAME\GUI\Assets\ActionResources_c\Icons\Resources\Highlight\ACTIONRESOURCENAME.DDS
+                    Mods\MODNAME\GUI\Assets\ActionResources_c\Icons\Resources\Missing\ACTIONRESOURCENAME.DDS
+                    Mods\MODNAME\GUI\Assets\ActionResources_c\Icons\Resources\Used\ACTIONRESOURCENAME.DDS
+            */
+            var modName = Path.GetFileName(modPathRoot);
+
+            // First pass just duplicate the image to the right size to all locations.
+            using Image<Rgba32> image = Image.Load<Rgba32>(inputPath);
+
+            List<Task> tasks = new List<Task>();
+
+            foreach (var setting in ActionResourceIconSettings)
+            {
+                string iconPath = string.Format(setting.IconPath, modName, actionResourceName);
+                if (setting.Optional) // Skipping for now.
+                {
+                    continue;
+                }
+
+                tasks.Add(image.GenerateIcon(setting.SideLength, Path.GetDirectoryName(iconPath)!, actionResourceName));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            return 0;
+        }
+
+    }
+}
--- Iconify/Program.cs
+++ Iconify/Program.cs
@@ -1,281 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using BG3Common;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
 
 namespace Iconify
 {
-    /// <summary>
-    /// Whether to include the "lslib_meta"="v1,bswap_guids" attribute for the xml doc.
-    /// </summary>
-    enum SwapTag
-    {
-        /// <summary>
-        /// Don't include the attribute.
-        /// </summary>
-        Absent,
-        /// <summary>
-        /// Include the attribute.
-        /// </summary>
-        Present
-    }
 
-    internal class Program
+    internal partial class Program
     {
         static int Main(string[] args)
         {
-            if(args.Length < 2) 
+            if(args.Length < 3) 
             {
-                Console.WriteLine("The input path of image files and the path to the module are required.");
+                Console.WriteLine("The operation type, input path of image file(s), and the path to the module are required.");
                 return -1;
             }
 
-            var inputPath = args[0];
-            var modPathRoot = args[1];
-            var modName = Path.GetFileName(modPathRoot);
+            var inputPath = args[1];
+            var modPathRoot = args[2];
 
-
-            // According to https://bg3.wiki/wiki/Modding:Creating_Item_Icons, we need to do multiple things:
-            // Public\Game\GUI\Assets\ControllerUIIcons\skills_png\Spell_Evocation_PulseWave.DDS
-            // Public\Game\GUI\Assets\Tooltips\Icons\Spell_Evocation_PulseWave.DDS
-            // Public\<mod>\GUI\Icons_Skills.lsx
-            // Path in Icons_Skills.lsx maps to path under Public\<mod>
-            /*
-	<region id="TextureAtlasInfo">
-		<node id="root">
-			<children>
-                <node id="TextureAtlasIconSize">
-                    <attribute id="Height" type="int32" value="64"/>
-                    <attribute id="Width" type="int32" value="64"/>
-                </node>
-                <node id="TextureAtlasPath">
-                    <attribute id="Path" type="string" value="Assets/Textures/Icons/Icons_5e_Spells.dds"/>
-                    <attribute id="UUID" type="FixedString" value="10b8d5a8-c07e-4822-9e74-34bf22692833"/>
-                </node>
-                <node id="TextureAtlasTextureSize">
-                    <attribute id="Height" type="int32" value="2048"/>
-                    <attribute id="Width" type="int32" value="2048"/>
-                </node>
-            </children>
-        </node>
-    </region>				  				
-            */
-
-            var imageFileNames = GatherImageFiles(inputPath);
-            if(imageFileNames.Length == 0)
+            switch(args[0].ToLowerInvariant())
             {
-                Console.WriteLine($"No images found in {inputPath}!");
-                return -1;
-            }
-
-            string controllerIconPath = Path.Combine(modPathRoot, "Public", "Game", "GUI", "Assets", "ControllerUIIcons", "skills_png");
-            string tooltipIconPath = Path.Combine(modPathRoot, "Public", "Game", "GUI", "Assets", "Tooltips", "Icons");
-            var skillsAtlasPartialPath = Path.Combine("Assets", "Textures", "Icons", modName.BG3Normalize() + "_Icons.DDS");
-            var uiRegistrationAtlasPartialPath = Path.Combine("Public", modName, skillsAtlasPartialPath);
-            var atlasPath = Path.Combine(modPathRoot, "Public", modName, "Assets", "Textures", "Icons");
-            var uiPath = Path.Combine(modPathRoot, "Public", "Game", "Content", "UI", "[PAK]_UI");
-            var skillsPath = Path.Combine(modPathRoot, "Public", modName, "GUI", "Icons_Skills.lsx");
-
-            Preclean(controllerIconPath, tooltipIconPath, atlasPath, uiPath, skillsPath);
-
-            var atlasAxisLength = Atlas.GetAtlasImageAxisLength(imageFileNames.Length);
-
-            Image<Rgba32> joined = new Image<Rgba32>(64 * atlasAxisLength, 64 * atlasAxisLength);
-            joined.Mutate(x => x.Opacity(0));
-
-            Dictionary<string, Point> imageMap = new Dictionary<string, Point>();
-            Size atlasImageSize = new Size(64, 64);
-            Point current = new Point(0, 0);
-
-            List<Task> tasks = new List<Task>();
-
-            foreach (var imageFileName in imageFileNames)
-            {
-                var imageName = Path.GetFileNameWithoutExtension(imageFileName);
-                using Image<Rgba32> image = Image.Load<Rgba32>(Path.Combine(inputPath, imageFileName));
-
-                tasks.Add(image.GenerateIcon(380, tooltipIconPath, imageName));
-                tasks.Add(image.GenerateIcon(144, controllerIconPath, imageName));
-
-                image.Mutate(x => x.Resize(64, 64));
-                joined.Mutate(x => x.DrawImage(image, current * 64, new GraphicsOptions()));
-
-                imageMap[imageName] = current;
-                current = new Point(current.X + 1, current.Y);
-                if(current.X == atlasAxisLength)
-                {
-                    current = new Point(0, current.Y + 1);
-                }
-            }
-
-            var atlasId = Guid.NewGuid();
-            joined.SaveDdsImage(Path.Combine(modPathRoot, "Public", modName, skillsAtlasPartialPath));
-            SaveImageMap(imageMap, atlasId, atlasAxisLength, skillsAtlasPartialPath, skillsPath);
-            SaveImageBank(atlasId, SwapTag.Present, uiRegistrationAtlasPartialPath, Path.Combine(uiPath, atlasId.ForBG3() + ".lsf.lsx"));
-            //SaveImageBank(atlasId, false, partialPath, Path.Combine(uiPath, "_merged.lsx"));
-
-            Task.WaitAll(tasks.ToArray());
-
-            return 0;
-        }
-
-        private static void Preclean(params string[] paths)
-        {
-            foreach(var path in paths)
-            {
-                if(File.Exists(path))
-                {
-                    File.Delete(path);
-                    continue;
-                }
-                if(Directory.Exists(path))
-                {
-                    Directory.Delete(path, true);
-                }
-            }
-        }
-
-        private static void SaveImageBank(Guid atlasId, SwapTag isPresent, string atlasPartialPath, string imageBankLsfFilePath)
-        {
-            Directory.CreateDirectory(Path.GetDirectoryName(imageBankLsfFilePath)!);
-            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
-            xmlWriterSettings.Indent = true;
-            xmlWriterSettings.Encoding = Encoding.UTF8;
-            using XmlWriter writer = XmlWriter.Create(imageBankLsfFilePath, xmlWriterSettings);
-            writer.WriteStartDocument();
-            using (var save = writer.WriteScopedElement("save"))
-            {
-                using (var version = writer.WriteScopedElement("version"))
-                {
-                    writer.WriteAttributeString("major", "4");
-                    writer.WriteAttributeString("minor", "0");
-                    writer.WriteAttributeString("revision", "0");
-                    writer.WriteAttributeString("build", "49");
-                    if(isPresent == SwapTag.Present)
+                // Example: atlas D:\GitHub\frm\bg3_mod_epic6\Icons D:\GitHub\frm\bg3_mod_epic6\DnD-Epic6
+                case "atlas":
+                    return CreateAtlas(inputPath, modPathRoot);
+                // Example: actionresource D:\GitHub\frm\bg3_mod_epic6\Icons\E6_Logo.png D:\GitHub\frm\bg3_mod_epic6\DnD-Epic6 FeatPoint
+                case "actionresource":
+                    if (args.Length < 4)
                     {
-                        writer.WriteAttributeString("lslib_meta", "v1,bswap_guids");
+                        Console.WriteLine("The operation type, input path of image file(s), the path to the module, and the name of the action resource (as specified in ActionResourceDefinitions.xml) are required.");
+                        return -1;
                     }
-                }
-                using(var region = writer.WriteScopedElement("region"))
-                {
-                    writer.WriteAttributeString("id", "TextureBank");
-                    using(var node = writer.WriteScopedElement("node"))
-                    {
-                        writer.WriteAttributeString("id", "TextureBank");
-                        using(var children = writer.WriteScopedElement("children"))
-                        {
-                            using(var resource = writer.WriteScopedElement("node"))
-                            {
-                                writer.WriteAttributeString("id", "Resource");
-                                writer.WriteLsxAttribute("ID", "FixedString", atlasId.ForBG3());
-                                writer.WriteLsxAttribute("Localized", false);
-                                writer.WriteLsxAttribute("Name", "LSString", Path.GetFileNameWithoutExtension(atlasPartialPath));
-                                writer.WriteLsxAttribute("SRGB", true);
-                                writer.WriteLsxAttribute("SourceFile", "LSString", atlasPartialPath.Replace('\\', '/'));
-                                writer.WriteLsxAttribute("Streaming", true);
-                                writer.WriteLsxAttribute("Template", "FixedString", "Icons_Skills");
-                                writer.WriteLsxAttribute("Type", 0);
-                                //writer.WriteLsxAttribute("_OriginalFileVersion_", "Int64", "0");
-                            }
-                        }
-                    }
-                }
+                    return CreateActionResourceIcons(inputPath, modPathRoot, args[3]);
+                default:
+                    Console.WriteLine("Unknown operation type: {0}", args[0]);
+                    return -1;
             }
-            writer.WriteEndDocument();
-        }
-
-        private static void SaveImageMap(Dictionary<string, Point> imageMap, Guid atlasId, int atlasAxisLength, string atlasPartialPath, string imageMapLsxFilePath)
-        {
-            Directory.CreateDirectory(Path.GetDirectoryName(imageMapLsxFilePath)!);
-            XmlWriterSettings xmlWriterSettings = new XmlWriterSettings();
-            xmlWriterSettings.Indent = true;
-            xmlWriterSettings.Encoding = Encoding.UTF8;
-            using XmlWriter writer = XmlWriter.Create(imageMapLsxFilePath, xmlWriterSettings);
-            writer.WriteStartDocument();
-            using (var save = writer.WriteScopedElement("save"))
-            {
-                using(var version = writer.WriteScopedElement("version"))
-                {
-                    writer.WriteAttributeString("major", "4");
-                    writer.WriteAttributeString("minor", "0");
-                    writer.WriteAttributeString("revision", "0");
-                    writer.WriteAttributeString("build", "49");
-                }
-
-                using(var region = writer.WriteScopedElement("region"))
-                {
-                    writer.WriteAttributeString("id", "IconUVList");
-                    using (var node = writer.WriteScopedElement("node"))
-                    {
-                        writer.WriteAttributeString("id", "root");
-                        using (var children = writer.WriteScopedElement("children"))
-                        {
-                            foreach (var image in imageMap)
-                            {
-                                Point p = image.Value;
-                                using (var iconUV = writer.WriteScopedElement("node"))
-                                {
-                                    writer.WriteAttributeString("id", "IconUV");
-
-                                    var (upperLeft, lowerRight) = p.GetUVs(atlasAxisLength);
-                                    writer.WriteLsxAttribute("MapKey", "FixedString", image.Key);
-                                    writer.WriteLsxAttribute("U1", upperLeft.U);
-                                    writer.WriteLsxAttribute("U2", lowerRight.U);
-                                    writer.WriteLsxAttribute("V1", upperLeft.V);
-                                    writer.WriteLsxAttribute("V2", lowerRight.V);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                using (var region = writer.WriteScopedElement("region"))
-                {
-                    writer.WriteAttributeString("id", "TextureAtlasInfo");
-                    using (var node = writer.WriteScopedElement("node"))
-                    {
-                        writer.WriteAttributeString("id", "root");
-                        using (var children = writer.WriteScopedElement("children"))
-                        {
-                            using (var TextureAtlasIconSize = writer.WriteScopedElement("node"))
-                            {
-                                writer.WriteAttributeString("id", "TextureAtlasIconSize");
-                                writer.WriteLsxAttribute("Height", 64);
-                                writer.WriteLsxAttribute("Width", 64);
-                            }
-                            using (var TextureAtlasPath = writer.WriteScopedElement("node"))
-                            {
-                                writer.WriteAttributeString("id", "TextureAtlasPath");
-                                writer.WriteLsxAttribute("Path", "string", atlasPartialPath.Replace('\\', '/'));
-                                writer.WriteLsxAttribute("UUID", "FixedString", atlasId.ForBG3());
-                            }
-                            using (var TextureAtlasTextureSize = writer.WriteScopedElement("node"))
-                            {
-                                writer.WriteAttributeString("id", "TextureAtlasTextureSize");
-                                var atlasSize = atlasAxisLength * 64;
-                                writer.WriteLsxAttribute("Height", atlasSize);
-                                writer.WriteLsxAttribute("Width", atlasSize);
-                            }
-                        }
-                    }
-                }
-            }
-            writer.WriteEndDocument();
-        }
-
-        private static readonly string[] mSupportedExtensions = [".png", ".gif", ".jpg", ".bmp", ".webp"];
-        static string[] GatherImageFiles(string path)
-        {
-            return Directory.EnumerateFiles(path)
-                .Where(file => mSupportedExtensions.Contains(Path.GetExtension(file).ToLowerInvariant()))
-                .ToArray();
         }
     }
 }
